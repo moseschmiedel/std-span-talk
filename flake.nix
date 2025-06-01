7cb392394a5ff457c1b06c1f47a51ce15eb5e27b
Mose Schmiedel <mose@schmiednet.de>
2025-06-02 01:28:35 +0200

    feat: Generate `compile_commands.json` with CMake

--- flake.nix
+++ flake.nix
@@ -12,22 +12,36 @@
 	    pname = "example";
             lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
             version = builtins.substring 0 8 lastModifiedDate;
+	    src = ./examples;
+
 	    pkgs = nixpkgs.legacyPackages.${system};
+	    depsBuildBuild = with pkgs; [ clang ninja cmake ];
+	    buildInputs = with pkgs; [ ];
+
+	    cmakeFlags = [ "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" ];
+	    cmakeBuildDir = "build";
+
 	    inherit (pkgs) stdenv;
         in
 	{
             packages = rec {
 	    	examples = stdenv.mkDerivation rec {
-                    inherit pname version;
-
-                    src = ./examples; 
-
-		    depsBuildBuild = with pkgs; [ clang ninja cmake ];
-
-		    cmakeBuildDir = "build";
+                    inherit pname version src depsBuildBuild buildInputs cmakeFlags cmakeBuildDir;
 	        };
 
 		default = examples;
+	    };
+
+	    devShells.default = pkgs.mkShell {
+	        inherit pname version src depsBuildBuild buildInputs cmakeBuildDir cmakeFlags;
+
+		name = "${pname}-${version}-shell";
+
+		shellHook = ''
+		    rm -rf ${cmakeBuildDir}
+		    mkdir -p ${cmakeBuildDir}
+		    cmake -B ${cmakeBuildDir} -S ${src} -G Ninja ${builtins.concatStringsSep " " cmakeFlags}
+		'';
 	    };
         });
 }
