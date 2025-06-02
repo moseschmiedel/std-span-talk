789741187208757676cf922a227d2fffa597ad1e
Mose Schmiedel <mose@schmiednet.de>
2025-06-02 17:04:49 +0200

    refactor: Add seperate function declaration for `info()`

--- examples/simple_function.cpp
+++ examples/simple_function.cpp
@@ -3,10 +3,10 @@
 #include <print>
 #include <string>
 
-const size_t MAX_SPACES = 27;
+template <typename T, size_t E>
+void info(std::string var_name, std::span<T, E> s);
 
 template <typename T, size_t E>
-
 void info(std::string var_name, std::span<T, E> s) {
     size_t spaces_amount = WIDTH_L - var_name.size();
     std::string spaces(spaces_amount, ' ');
