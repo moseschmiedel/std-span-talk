fb9efe7781e8e3e16a7efdfbc85a4b74e0e000e1
Mose Schmiedel <mose@schmiednet.de>
2025-06-02 16:43:59 +0200

    feat: Add table header

--- examples/main.cpp
+++ examples/main.cpp
@@ -13,28 +13,32 @@ const size_t WIDTH_R = 12;
 #include "simple_function.cpp"
 
 int main() {
-    std::println("std::vector");
+    std::println("std::vector:");
     table_limit();
+    table_header("Constructor", "Extent", "data");
     #include "vector.cpp"
     table_limit();
     std::println();
 
-    std::println("std::array");
+    std::println("std::array:");
     table_limit();
+    table_header("Constructor", "Extent", "data");
     #include "array.cpp"
     table_limit();
     std::println();
 
 
-    std::println("C-style array");
+    std::println("C-style array:");
     table_limit();
+    table_header("Constructor", "Extent", "data");
     #include "c_array.cpp"
     table_limit();
     std::println();
 
 
-    std::println("iterators");
+    std::println("Iterators:");
     table_limit();
+    table_header("Constructor", "Extent", "data");
     #include "iterator.cpp"
     table_limit();
 
--- examples/table.cpp
+++ examples/table.cpp
@@ -4,3 +4,15 @@ void table_limit() {
     std::string dashes_r(WIDTH_R + 2, '-');
     std::println("+{}+{}+{}+", dashes_l, dashes_c, dashes_r);
 }
+
+void table_header(std::string title_l, std::string title_c, std::string title_r) {
+    std::string dashes_l(WIDTH_L + 2, '=');
+    std::string dashes_c(WIDTH_C + 2, '=');
+    std::string dashes_r(WIDTH_R + 2, '=');
+    std::string spaces_l(WIDTH_L - title_l.size(), ' ');
+    std::string spaces_r(WIDTH_R - title_r.size(), ' ');
+    std::string spaces_c(WIDTH_C - title_c.size(), ' ');
+
+    std::println("| {}{} | {}{} | {}{} |", title_l, spaces_l, title_c, spaces_c, title_r, spaces_r);
+    std::println("+{}+{}+{}+", dashes_l, dashes_c, dashes_r);
+}
