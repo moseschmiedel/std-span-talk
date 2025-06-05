c9bb45122f06004a0079957bfc651106cbd09d50
Mose Schmiedel <mose@schmiednet.de>
2025-06-05 15:31:17 +0200

    feat: Add `apps` section to `flake.nix` to execute binaries via `nix run` CLI

--- flake.nix
+++ flake.nix
@@ -1,56 +1,100 @@
 {
-    description = "Presentation and code examples for talk over `std::span`";
+  description = "Presentation and code examples for talk over `std::span`";
 
-    inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
-      flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, flake-utils }:
-	flake-utils.lib.eachDefaultSystem (system: 
-        let 
-	    pname = "std-span-examples";
-            lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-            version = builtins.substring 0 8 lastModifiedDate;
-	    src = ./examples;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pname = "std-span-examples";
+        lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+        version = builtins.substring 0 8 lastModifiedDate;
+        src = ./examples;
 
-	    pkgs = nixpkgs.legacyPackages.${system};
-	    depsBuildBuild = with pkgs; [ clang ninja cmake ];
-	    buildInputs = with pkgs; [ ];
+        pkgs = nixpkgs.legacyPackages.${system};
+        depsBuildBuild = with pkgs; [
+          clang
+          ninja
+          cmake
+        ];
+        buildInputs = with pkgs; [ ];
 
-	    cmakeFlags = [ "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" ];
-	    cmakeBuildDir = "build";
+        cmakeFlags = [ "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" ];
+        cmakeBuildDir = "build";
 
-	    inherit (pkgs) stdenv;
-        in
-	{
-            packages = rec {
-	    	std-span-construction = stdenv.mkDerivation rec {
-                    inherit pname version src depsBuildBuild buildInputs cmakeFlags cmakeBuildDir;
-	        };
-	    	std-span-dangling-span = stdenv.mkDerivation rec {
-                    inherit pname version src depsBuildBuild buildInputs cmakeFlags cmakeBuildDir;
-	        };
-	    	std-span-my-container = stdenv.mkDerivation rec {
-                    inherit pname version src depsBuildBuild buildInputs cmakeFlags cmakeBuildDir;
-	        };
-	    	std-span-mdspan = stdenv.mkDerivation rec {
-                    inherit pname version src depsBuildBuild buildInputs cmakeFlags cmakeBuildDir;
-	        };
+        inherit (pkgs) stdenv;
+      in
+      {
+		apps = {
+			construction = {
+				type = "app";
+				program = "${self.packages.${system}.std-span-examples}/bin/std-span-construction";
+				meta.description = "C++23 example showcasing several ways to construct a std::span.";
+			};
 
-		default = std-span-construction;
-	    };
+			dangling-span = {
+				type = "app";
+				program = "${self.packages.${system}.std-span-examples}/bin/std-span-dangling-span";
+				meta.description = "C++23 example showcasing the danger of a dangling std::span.";
+			};
 
-	    devShells.default = pkgs.mkShell {
-	        inherit pname version src depsBuildBuild buildInputs cmakeBuildDir cmakeFlags;
+			my-container = {
+				type = "app";
+				program = "${self.packages.${system}.std-span-examples}/bin/std-span-my-container";
+				meta.description = "C++23 example showcasing how to implement conversion from custom container type (`MyContainer`) to std::span.";
+			};
 
-		name = "${pname}-${version}-shell";
+			mdspan = {
+				type = "app";
+				program = "${self.packages.${system}.std-span-examples}/bin/std-span-mdspan";
+				meta.description = "C++23 example showcasing a simple std::mdspan instantiation and member access.";
+			};
+		};
 
-		shellHook = ''
-		    rm -rf ${cmakeBuildDir}
-		    mkdir -p ${cmakeBuildDir}
-		    cmake -B ${cmakeBuildDir} -S ${src} -G Ninja ${builtins.concatStringsSep " " cmakeFlags}
-		'';
-	    };
-        });
+        packages = rec {
+          std-span-examples = stdenv.mkDerivation rec {
+            inherit
+              pname
+              version
+              src
+              depsBuildBuild
+              buildInputs
+              cmakeFlags
+              cmakeBuildDir
+              ;
+          };
+
+          default = std-span-examples;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit
+            pname
+            version
+            src
+            depsBuildBuild
+            buildInputs
+            cmakeBuildDir
+            cmakeFlags
+            ;
+
+          name = "${pname}-${version}-shell";
+
+          shellHook = ''
+            		    rm -rf ${cmakeBuildDir}
+            		    mkdir -p ${cmakeBuildDir}
+            		    cmake -B ${cmakeBuildDir} -S ${src} -G Ninja ${builtins.concatStringsSep " " cmakeFlags}
+            		'';
+        };
+      }
+    );
 }
