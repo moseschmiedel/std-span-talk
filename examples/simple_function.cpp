bc07cbae5a79ec1d2580c98c3a7b0c2099d48480
Mose Schmiedel <mose@schmiednet.de>
2025-06-23 23:59:41 +0200

    feat: Add implementation of function `f` in simple_function.cpp

--- examples/simple_function.cpp
+++ examples/simple_function.cpp
@@ -1,9 +1,13 @@
+#include <print>
 #include <span>
 
-// simple (fixed) span usage 
+// simple (fixed) span usage
 void foo(std::span<int, 10> s);
 void bar(std::span<int, std::dynamic_extent> s);
 
-// generic span usage 
-template <typename T, size_t E>
-void f(std::span<T, E> s);
+// generic span usage
+template <typename T, size_t E> void f(std::span<T, E> s);
+
+template <typename T, size_t E> void f(std::span<T, E> s) {
+  std::println("{}", s);
+}
