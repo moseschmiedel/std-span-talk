4eb0bb90082a64ba185d8d44224da69996182e81
Mose Schmiedel <mose@schmiednet.de>
2025-06-24 11:01:42 +0200

    feat: Extend mdspan example for better understanding of higher dimensions (> 2d)

--- examples/mdspan.cpp
+++ examples/mdspan.cpp
@@ -1,13 +1,19 @@
+#include <mdspan>
 #include <print>
 #include <vector>
-#include <mdspan>
 
 int main() {
-  std::vector d{1,0,0,1};
+  std::vector d{1, 0, 0, 1};
 
   auto m2by2 = std::mdspan(d.data(), 2, 2);
   auto m2by1by2 = std::mdspan(d.data(), 2, 1, 2);
 
-  std::println("{}", m2by2[1,1]);
-  std::println("{}", m2by1by2[1,0,1]);
+  std::println("{}", m2by2[1, 1]);
+  for (std::size_t i = 0; i < m2by1by2.extent(0); i++) {
+    for (std::size_t j = 0; j < m2by1by2.extent(1); j++) {
+      for (std::size_t k = 0; k < m2by1by2.extent(2); k++) {
+        std::print("{},", m2by1by2[i, j, k]);
+      }
+    }
+  }
 }
